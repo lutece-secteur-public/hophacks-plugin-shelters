3153e5d8e95c9bd648535b628648c3325733ab43
Philippe Bareille <philippe.bareille@paris.fr>
2018-09-15 18:17:19 -0400

    init data

--- src/sql/plugins/shelters/plugin/create_db_shelters.sql
+++ src/sql/plugins/shelters/plugin/create_db_shelters.sql
@@ -7,15 +7,15 @@ DROP TABLE IF EXISTS shelters_shelter;
 CREATE TABLE shelters_shelter (
 id_shelter int AUTO_INCREMENT,
 name varchar(50) default '' NOT NULL,
-description varchar(255) default '',
+description varchar(500) default '',
 email varchar(255) default '' NOT NULL,
 web_site varchar(255) default '',
 workgroup_key varchar(50) default '' NOT NULL,
 reminder_status SMALLINT,
 bed_capacity int default '0',
 phone_number varchar(50) default '',
-location_lat int default '0',
-location_lon int default '0',
+location_lat float(11) default '0',
+location_lon float(11) default '0',
 address long varchar,
 PRIMARY KEY (id_shelter)
 );
