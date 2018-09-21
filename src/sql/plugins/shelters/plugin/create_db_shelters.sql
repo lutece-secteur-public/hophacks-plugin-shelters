8481244429d23f90c085d7726db92c57a063a341
Philippe Bareille <philippe.bareille@paris.fr>
2018-09-21 06:32:07 -0400

    added real data to db init scripts

--- src/sql/plugins/shelters/plugin/create_db_shelters.sql
+++ src/sql/plugins/shelters/plugin/create_db_shelters.sql
@@ -6,7 +6,7 @@
 DROP TABLE IF EXISTS shelters_shelter;
 CREATE TABLE shelters_shelter (
 id_shelter int AUTO_INCREMENT,
-name varchar(50) default '' NOT NULL,
+name varchar(100) default '' NOT NULL,
 description varchar(500) default '',
 email varchar(255) default '' NOT NULL,
 web_site varchar(255) default '',
@@ -18,18 +18,4 @@ location_lat float(11) default '0',
 location_lon float(11) default '0',
 address long varchar,
 PRIMARY KEY (id_shelter)
-);
-
-
---
--- Structure for table shelters_bed_availability
---
-
-DROP TABLE IF EXISTS shelters_bed_availability;
-CREATE TABLE shelters_bed_availability (
-id_shelter int  default '0' NOT NULL,
-date_code varchar(50) default '' NOT NULL,
-bed_available_count int default '0' NOT NULL,
-total_bed_capacity int default '0' NOT NULL,
-PRIMARY KEY (id_shelter , date_code )
 );
