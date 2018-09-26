13cb7cb8c883113f236b807495908ca3b1da56ec
Laurent HOHL <laurent.hohl@gmail.com>
2018-09-26 17:27:16 +0200

    Mise à jour template back + js ajout couleur gray

--- webapp/js/plugins/shelters/sheltersMap.js
+++ webapp/js/plugins/shelters/sheltersMap.js
@@ -1,5 +1,4 @@
-var greenIcon;
-var redIcon ;
+var greenIcon, redIcon, grayIcon ;
     
 function initShelterMap(lat, lon, zoom) {
     var map = new L.Map( 'map' );
@@ -21,6 +20,15 @@ function initShelterMap(lat, lon, zoom) {
 
     redIcon = new L.Icon({
         iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    });
+
+    grayIcon = new L.Icon({
+        iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-black.png',
         shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
         iconSize: [25, 41],
         iconAnchor: [12, 41],
